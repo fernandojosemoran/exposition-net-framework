00f49483f10bfea029a9c5774ded81d733aced10
Fernando Moran <fernandomoran323@yahoo.es>
2023-10-10 23:32:40 -0600

    tailwind, basic content, build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,7 +31,19 @@ const config = {
     defaultLocale: 'es',
     locales: ['es'],
   },
-
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
   presets: [
     [
       'classic',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import clsx from 'clsx';
-// import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import styles from './index.module.css';
 
 function HomepageHeader() {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className='bg-emerald-600 flex flex-col items-center text-center gap-10 p-10'>
       {/* <img src=''>QUE ES .NET FRAMEWORK</img> */}
-      <h1 className='text-center'>QUE ES .NET FRAMEWORK</h1>
+      <h1 className='xl:text-4xl text-lg md:text-xl'>QUE ES .NET FRAMEWORK</h1>
+      <div className='xl:w-1/2 text-sm md:text-sm lg:text-xl xl:text-4xl'>
+        <p>Es una plataforma de desarrollo para crear aplicaciones para web, Windows y Microsoft Azure.</p>
+      </div>
     </header>
   );
 }
@@ -22,9 +21,23 @@ export default function Home(): JSX.Element {
       title={siteConfig.title}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-          <h2> </h2>
-      </main>
+      <div className='md:px-10 lg:px-16 xl:px-20'>
+        <main className='pt-10 flex flex-col gap-20 p-5'>
+          <section className='flex flex-col gap-4'>
+            <h2 className='text-lg xl:text-2xl font-bold'>Que tipos de proyectos se pueden crear con <span>.Net Framework</span>?</h2>
+            <div className=' text-sm text-md md:text-lg lg:text-xl'>
+              <li className='list-disc'>Backend usando <span className='font-sans'>ASP.NET Core, and .NET</span>.</li>
+              <li className='list-disc'>Frontend usando.</li>
+              <li className='list-disc'>Aplicaciones de escritorio usando <span className='font-sans'>.NET Core, .NET Framework, .Net</span>.</li>
+              <li className='list-disc'>Trabajar con datos usando <span className='font-sans'>ADO.NET</span>.</li>
+              <li className='list-disc'>Aplicaciones de consola.</li>
+            </div>
+          </section>
+          <section>
+            <h2 className='text-lg xl:text-2xl font-bold'>Caracteristicas de <span className='font-semibold'>.Net Framework</span></h2>
+          </section>
+        </main>
+      </div>
     </Layout>
   );
 }
